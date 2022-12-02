521a19aac95e5be4d84edbc56a8afa9f958437c3
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-12-02 13:18:59 +0000

    add more interfaces for props

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,22 @@
 import { Todo } from "./Todo";
-import { ITodoItem } from "../utils/interfaces";
+import { IEditTodoItem, ITodoItem } from "../utils/interfaces";
 
 interface TodoListProps {
   todoItems: ITodoItem[];
+  handleEditTicket: (ticket:ITodoItem)=>void
 }
 
-export function TodoList({ todoItems }: TodoListProps): JSX.Element {
+export function TodoList({ todoItems, handleEditTicket }: TodoListProps): JSX.Element {
   return (
     <div>
       {todoItems.map((el: ITodoItem) => (
         <div key={el.id}>
-          <Todo todoElement={el} />
+          <Todo todoElement={el} handleEditTicket={handleEditTicket}/>
         </div>
       ))}
       {todoItems.map((el: ITodoItem) => (
         <div key={el.id}>
-          <Todo todoElement={el} />
+          <Todo todoElement={el} handleEditTicket={handleEditTicket}/>
         </div>
       ))}
     </div>
